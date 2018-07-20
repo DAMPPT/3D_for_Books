ccf4d098f715f85d15d953af87c02a43511f9670
DAMPPT <41467914+DAMPPT@users.noreply.github.com>
2018-07-20 10:45:42 +0100

    Commit1

---
+++ Code/test/Assets/Scripts/RotateObject1.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RotateObject1 : MonoBehaviour
+{
+    public float rotateSpeed = 50f;
+    bool rotateStatus = false;
+    private GameObject[] spheres;
+
+
+    void Start()
+    {
+        spheres = GameObject.FindGameObjectsWithTag("Sphere");
+    }
+
+    public void Rotasi()
+    {
+
+        if (rotateStatus == false)
+        {
+            rotateStatus = true;
+        }
+        else
+        {
+            rotateStatus = false;
+        }
+    }
+
+    void Update()
+    {
+        if (rotateStatus == true)
+        {
+            foreach (GameObject objectRotate in spheres)
+            {
+                objectRotate.transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+            }
+           
+        }
+    }
+}
